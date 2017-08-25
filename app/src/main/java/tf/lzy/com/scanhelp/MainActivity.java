cc0385d125c79acac5f679aa25400ce322194ed3
King <624771244@qq.com>
2017-08-25 13:50:55 +0800

    增加了新类

--- app/src/main/java/tf/lzy/com/scanhelp/MainActivity.java
+++ app/src/main/java/tf/lzy/com/scanhelp/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private String name;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/tf/lzy/com/scanhelp/TestActivity.java
@@ -0,0 +1,8 @@
+package tf.lzy.com.scanhelp;
+
+/**
+ * Created by Thinkpad on 2017/8/25.
+ */
+
+public class TestActivity {
+}
